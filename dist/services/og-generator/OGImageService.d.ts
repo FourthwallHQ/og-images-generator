626b8cf49e3420c2c9f7aec96795dbfc5b810e5e
Michal <michal.lester@fourthwall.com>
2025-09-12 09:21:56 +0200

    remove dist

--- dist/services/og-generator/OGImageService.d.ts
+++
@@ -1,7 +0,0 @@
-import { ImageResponse } from '@vercel/og';
-import { OGImageShopRequest } from './schemas';
-export declare class OGImageService {
-    static generateShopImage({ offerImagesUrls, stylesUrl, logoUrl, shopName, poweredBy, }: OGImageShopRequest): Promise<ImageResponse>;
-    static generateShopImageBuffer(params: OGImageShopRequest): Promise<Buffer>;
-}
-//# sourceMappingURL=OGImageService.d.ts.map
